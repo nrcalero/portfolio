9cbd72e3130b2a6ecf26fb2ff061457c4647f092
Nathanyel Calero <nrcalero@gmail.com>
2022-08-26 13:23:26 -0700

    Updates

---
+++ precache-manifest.04fa3d4f4715cefa498cd06702ebc8aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "47606983b546da8db085",
+    "url": "/portfolio/static/js/main.47606983.chunk.js"
+  },
+  {
+    "revision": "25c89e777bfafba98a6d",
+    "url": "/portfolio/static/js/1.25c89e77.chunk.js"
+  },
+  {
+    "revision": "47606983b546da8db085",
+    "url": "/portfolio/static/css/main.4573b417.chunk.css"
+  },
+  {
+    "revision": "125410f7304bf62ece2c57ba88741bd0",
+    "url": "/portfolio/index.html"
+  }
+];
